c4b4f660692b491356adc43c5821a16e193c8b36
HelloBro89 <pavelvaloshyn89@gmail.com>
2021-07-06 23:23:49 +0200

    feat: fix bags and refactoring code

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,7 +1,5 @@
 import { JwtService } from '@nestjs/jwt';
 import { Injectable, CanActivate, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
-// import { config } from '../common/ormconfig';
-// import { configOlder } from '../common/config';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -9,26 +7,25 @@ export class AuthGuard implements CanActivate {
     constructor(private jwtService: JwtService) { };
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        const req = context.switchToHttp().getRequest();
-        try {
-            const sessionToken = req.headers.authorization;
+        const request = context.switchToHttp().getRequest();
 
-            const type = sessionToken.split(' ')[0];
-            const token = sessionToken.split(' ')[1];
+        try {
+            const sessionToken = request.headers.authorization;
+            if (!sessionToken) {
+                throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+            }
+            const [type, token] = sessionToken.split(' ');
+
             if (type !== 'Bearer' || !token) {
                 throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
             }
-
-            /* const receivedUser =  */this.jwtService.verify(token);
-            // console.log(receivedUser);
+            this.jwtService.verify(token);
             return true;
 
         } catch (err) {
-            console.log(err);
             throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
         }
     }
-
 }
 
 
